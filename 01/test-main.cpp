40efed9f8efbdc4a44646a3f940a13d530f8aac7
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-14 00:53:34 +0300

    initial commit

---
+++ 01/test-main.cpp
@@ -0,0 +1,27 @@
+﻿#include "main.cpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+
+
+TEST_CASE("Empty", "[Check empty list]") {
+    List test_list;
+    SECTION("Тестирование методов двусвязного списка") {
+        CHECK(test_list.Size() == 0);
+        test_list.PushBack(20);
+        CHECK(test_list.Size() == 1);
+        test_list.PushBack(21);
+        CHECK(test_list.Size() == 2);
+        test_list.PopBack();
+        CHECK(test_list.Size() == 1);
+    }
+
+    SECTION("Проверка очистки списка") {
+        test_list.Clear();
+        CHECK(test_list.Size() == 0);
+    }
+}
+
+int main(int argc, char** argv)
+{   
+    return Catch::Session().run(argc, argv);
+}
