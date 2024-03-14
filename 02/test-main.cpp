4904c0459d23900d868b4a9227c5f51fdc5be2cf
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-14 03:53:04 +0300

    second mission is completed

---
+++ 02/test-main.cpp
@@ -0,0 +1,28 @@
+﻿#include "main.cpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+
+
+TEST_CASE("Empty", "[Check empty list]") {
+    List test_list;
+    SECTION("Тестирование списка на пустоту") {
+        CHECK(test_list.Empty() == true);
+        CHECK(test_list.Size() == 0);
+        test_list.PushBack(20);
+        CHECK(test_list.Size() == 1);
+        test_list.PushBack(21);
+        CHECK(test_list.Size() == 2);
+        test_list.PopBack();
+        CHECK(test_list.Size() == 1);
+    }
+
+    SECTION("Проверка очистки списка") {
+        test_list.Clear();
+        CHECK(test_list.Size() == 0);
+    }
+}
+
+int main(int argc, char** argv)
+{   
+    return Catch::Session().run(argc, argv);
+}
